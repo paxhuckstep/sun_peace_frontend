8d0d24458d3b4fdd17bb42c8f2d37c6022dfb521
Pax Huckstep <paxhuckstep@gmail.com>
2026-06-14 20:34:05 -0600

    cart is empty display added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,11 +5,26 @@ import { productsData } from "../../constants";
 import CartCard from "../CartCard/CartCard";
 
 function Cart({ creamyCount, crunchyCount, editCount }) {
+  if (crunchyCount < 1 && creamyCount < 1) {
+    return (
+      <section className="cart">
+        <p className="cart__empty">Cart is Empty :/</p>
+      </section>
+    );
+  }
   return (
     <section className="cart">
       <ul className="cart__cards">
-        <CartCard productData={productsData[0]} productCount={creamyCount} editCount={editCount} />
-        <CartCard productData={productsData[1]} productCount={crunchyCount} editCount={editCount} />
+        <CartCard
+          productData={productsData[0]}
+          productCount={creamyCount}
+          editCount={editCount}
+        />
+        <CartCard
+          productData={productsData[1]}
+          productCount={crunchyCount}
+          editCount={editCount}
+        />
       </ul>
     </section>
   );
